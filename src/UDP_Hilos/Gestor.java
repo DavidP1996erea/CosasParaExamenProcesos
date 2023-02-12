9dca88c5ae0e86e44389ee328038203465dbb49f
David Perea <david.perea@iesnervion.es>
2023-02-12 19:09:48 +0100

    normal

---
+++ src/UDP_Hilos/Gestor.java
@@ -0,0 +1,59 @@
+package UDP_Hilos;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Gestor extends Thread {
+    DatagramSocket socket;
+    DatagramPacket datagramaEntrada;
+    int numeroAleatorio;
+
+
+    public Gestor(int numeroAleatorio, DatagramSocket socket, DatagramPacket datagramaEntrada) {
+        super();
+        this.socket = socket;
+        this.datagramaEntrada = datagramaEntrada;
+        this.numeroAleatorio = numeroAleatorio;
+    }
+
+    @Override
+    public void run() {
+        realizarProceso();
+    }
+
+    /**
+     * Método que recoge el mensaje recibido y lo convierte en entero, luego se comprueba
+     * que ese número sea el mismo que el generado aleatoriamente, en cada caso se le irá
+     * informando al usuario si el número es mayor o menor.
+     */
+    public void realizarProceso() {
+
+
+        // Recepción de mensaje del cliente
+        String mensajeRecibido = new String(datagramaEntrada.getData()).trim();
+        int numero = Integer.parseInt(mensajeRecibido);
+
+
+        // 5 - Generación y envío de la respuesta
+        System.out.println("(Ejercicio1UDP.Servidor): Enviando datagrama...");
+        byte[] mensajeEnviado = null;
+
+        if (numero == numeroAleatorio) {
+            mensajeEnviado = new String("Has adivinado el número").getBytes();
+
+        } else if (numero < numeroAleatorio) {
+            mensajeEnviado = new String("El numero es menor que el aleatorio").getBytes();
+        } else if (numero > numeroAleatorio) {
+            mensajeEnviado = new String("El numero es mayor que el aleatorio").getBytes();
+
+        }
+        DatagramPacket packetSalida = new DatagramPacket(mensajeEnviado, mensajeEnviado.length,
+                datagramaEntrada.getAddress(), datagramaEntrada.getPort());
+        try {
+            socket.send(packetSalida);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
